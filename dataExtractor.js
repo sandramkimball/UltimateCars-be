b5ca58cfc5bde28ce641289c9c22ea3e9ee2c50d
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2020-09-29 18:23:48 -0700

    changed allMakes 2D array to array of obj

--- dataExtractor.js
+++ dataExtractor.js
@@ -1,5 +1,6 @@
 
-module.exports = function dataExtractor(data) {
+// module.exports = function dataExtractor(data) {
+function dataExtractor(data) {
     let stats = []
 
     /* Callback Func: This returns an array of all models belonging to a car make.
@@ -25,17 +26,27 @@ module.exports = function dataExtractor(data) {
 
         for ( k in data ){
             let car = data[k] // data[1] = {make: 'Dodge', model: 'Viper', ...} 
-            var make = car['make']
+            let make = car['make']
 
             // If car make is not in allMakes:
             if ( !allMakes.has( make ) ){     // .has() only works on Map()       
                 // Create and add new object {'make', [models]}
-                allMakes.set( make, getModelsForEachMake(data, make) ) 
+                allMakes.set(make, getModelsForEachMake(data, make));
             }
         }
 
+        // Convert to 2D array 
         allMakes = Array.from( allMakes )
-        return allMakes
+
+        // Convert each sub array to an Object
+        let newAllMakes = allMakes.map( car => {
+            let newCarObj = new Object();
+            newCarObj[car[0]] = car[1]
+            car = newCarObj;
+            return car;
+        })
+
+        return newAllMakes
     };
 
     const getYears = ( data ) => {
@@ -80,8 +91,41 @@ module.exports = function dataExtractor(data) {
         throw new Error( 'Error extracting data.' )
     }
         
+    console.log(stats)
     return stats
 }
 
+let mock = [
+    {
+        make: 'Dodge',
+        model: 'Viper',
+        year: 2000,
+        price: 50
+    },
+    {
+        make: 'Porche',
+        model: 'Carrera',
+        year: 1980,
+        price: 500
+    },
+    {
+        make: 'Porche',
+        model: 'Carrera',
+        year: 1979,
+        price: 589
+    },
+    {
+        make: 'Mazda',
+        model: 'M3',
+        year: 2040,
+        price: 100
+    },{
+        make: 'Mazda',
+        model: 'Miata',
+        year: 2001,
+        price: 51
+    }
+]
 
+dataExtractor(mock)
 
